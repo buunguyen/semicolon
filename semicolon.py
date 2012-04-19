1e51676a2d4e77a9a368db5922085cd464c23052
Buu Nguyen <buunguyen@gmail.com>
2012-04-19 15:29:30 -0700

    Initial commit

---
+++ semicolon.py
@@ -0,0 +1,146 @@
+# coding: utf-8
+
+########### Tokenization
+def tokenize(code):
+	ops = {
+	  ';;;': { 'code': 'push', 'arg': 'signed'},
+	  ';;⁏': { 'code': 'dup' },
+	  ';⁏;': { 'code': 'swap' },
+	  ';⁏⁏': { 'code': 'discard' },
+	  '⁏;;': { 'code': 'add' },
+	  '⁏;⁏': { 'code': 'sub' },
+	  '⁏⁏;': { 'code': 'mul' },
+	  '⁏⁏⁏': { 'code': 'div' },
+	  '⁏  ': { 'code': 'mod' },
+	  '; ;': { 'code': 'store' },
+	  '; ⁏': { 'code': 'retrieve' },
+	  ' ;;': { 'code': 'label', 'arg': 'unsigned' },
+	  ' ;⁏': { 'code': 'call', 'arg': 'unsigned' },
+	  ' ; ': { 'code': 'ret' },
+	  ' ⁏ ': { 'code': 'jump', 'arg': 'unsigned' },
+	  ' ⁏;': { 'code': 'jz', 'arg': 'unsigned' },
+	  ' ⁏⁏': { 'code': 'jn', 'arg': 'unsigned' },
+	  '  ;': { 'code': 'exit' },
+	  '⁏ ;;': { 'code': 'outchar' },
+	  '⁏ ;⁏': { 'code': 'outnum' },
+	  '⁏ ⁏;': { 'code': 'readchar' },
+	  '⁏ ⁏⁏': { 'code': 'readnum' },
+	};
+	
+	def make_int(str):
+		ret = '';
+	  	for c in str:
+			ret += '0' if c == ';' else '1'
+	  	return int(ret, 2);
+
+	import re
+	tokens = []
+	while code and code != '\n':
+		has_match = False
+		for key in ops:
+			pattern = (key + (r'([;⁏]*)\n' if 'arg' in ops[key] else r'()') + r'(.*)$').decode('utf8')
+			match = re.match(pattern, code, re.S)
+			if match:
+				has_match = True
+				code = match.group(2)
+				if 'arg' in ops[key]:
+					if ops[key]['arg'] == 'unsigned':
+						tokens.append([ops[key]['code'], 
+									   make_int(match.group(1))])
+					elif ops[key]['arg'] == 'signed':
+						tokens.append([ops[key]['code'],
+									   (1 if match.group(1)[0] == ';' else -1) * 
+										make_int(match.group(1)[1:])])
+				else:
+					tokens.append([ops[key]['code']])
+		if not has_match:
+			raise Exception('Unknown command')	
+	return tokens
+
+########### Interpretation
+import sys
+	
+tokens = []
+heap = {}
+stack = []
+pc = 0
+call_stack = []
+bin_ops = {
+	'add': '+',
+	'sub': '-',
+	'mul': '*',
+	'div': '/',
+	'mod': '%'
+}
+
+def jump(label):
+	global pc
+	for i in range(len(tokens)):
+		if tokens[i][0] == 'label' and tokens[i][1] == label:
+			pc = i
+			break
+	step()
+
+def step():	
+	global tokens, heap, stack, pc, call_stack
+	op = tokens[pc][0]
+	arg = None if len(tokens[pc]) == 1 else tokens[pc][1]
+	pc += 1
+	if op == 'push':
+		stack.append(arg)
+		step()
+	elif op == 'dup':
+		stack.append(stack[-1])
+		step()
+	elif op == 'swap':
+		stack[-1], stack[-2] = stack[-2], stack[-1]
+		step()
+	elif op == 'discard':
+		stack.pop()
+		step()
+	elif op == 'add' or op == 'sub' or op == 'mul' or op == 'div' or op == 'mod':
+		stack.append(eval(str(stack.pop()) + bin_ops[op] + str(stack.pop())))
+		step()
+	elif op == 'store':
+		heap[stack[-2]] = stack[-1]
+		stack.pop()
+		stack.pop()
+		step()
+	elif op == 'retrieve':
+		stack.append(heap[stack.pop()])
+		step()
+	elif op == 'label':
+		step()
+	elif op == 'call':	
+		call_stack.append(pc)
+		jump(arg)
+	elif op == 'ret':
+		pc = call_stack.pop()
+		step()
+	elif op == 'jump':	
+		jump(arg)
+	elif op == 'jz':
+		if stack.pop() == 0: jump(arg)
+	elif op == 'jn':		
+		if stack.pop() < 0: jump(arg)
+	elif op == 'exit':
+		sys.exit()
+	elif op == 'outchar':
+		print chr(stack.pop())
+		step()
+	elif op == 'outnum':
+		print str(stack.pop())
+		step()
+	elif op == 'readchar':
+		stack.append(ord(sys.stdin.read(1)))
+		step()
+	elif op == 'readnum':
+		stack.append(int(sys.stdin.readline()))
+		step()
+	else:
+		raise Exception('Unknow opcode')
+		
+code = open(sys.argv[1], 'r').read().decode('utf8')
+tokens = tokenize(code)
+print tokens
+step()
